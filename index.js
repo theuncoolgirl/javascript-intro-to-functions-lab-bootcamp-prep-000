e1bc671dc1e173c17099afaa97b61b691f1ecfd9
Erin Shields <elshields92@gmail.com>
2020-04-14 01:53:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,8 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-
+function sayHiToGrandma(string) {
+  if (string === 'I love you, Grandma.') {
+    return `I love you, too.`
+  }
+}
