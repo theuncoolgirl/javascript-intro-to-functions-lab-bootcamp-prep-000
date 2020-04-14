6bc7560d97edf054240794d7c6442b5bfbeb3a3b
Erin Shields <elshields92@gmail.com>
2020-04-14 01:35:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+function sayHiToGrandma(string){
+  if (string.toUpperCase === string) {
+    return `YES INDEED!`
+  } else if (string.toLowerCase === string) {
+    return `I can't hear you!`
+  } else {
+    return `I love you, Grandma.`}
+}
