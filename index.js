f66ba5b4d20e98aa0f1d90f32b6ddeb528454169
Erin Shields <elshields92@gmail.com>
2020-04-14 01:56:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function sayHiToGrandma(string) {
   if (string === 'I love you, Grandma.') {
     return `I love you, too.`
   }
-  else if (string.toLowerCase === string) {
-    return `I can't hear you!`
+  else if (string.toUpperCase === string) {
+    return `YES INDEED!`
   }
   else {
-    return `YES INDEED!`
+    return `I can't hear you!`
   }
 }
